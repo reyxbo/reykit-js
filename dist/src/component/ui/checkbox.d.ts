2765abf6dd706f16531827999032189fe7c5fb42
ReyXBo <reyxbo@163.com>
2026-09-11 01:09:19 +0800

    Update.

--- dist/src/component/ui/checkbox.d.ts
+++
@@ -1,3 +0,0 @@
-import { Checkbox as CheckboxPrimitive } from '@base-ui/react/checkbox';
-declare function Checkbox({ className, ...props }: CheckboxPrimitive.Root.Props): import("react/jsx-runtime").JSX.Element;
-export { Checkbox };
--- dist/src/component/ui/icon.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentProps } from 'react';
-export * from 'lucide-react';
-export declare const Python: ({ ...props }: ComponentProps<"div">) => import("react/jsx-runtime").JSX.Element;
-export declare const Javascript: ({ ...props }: ComponentProps<"div">) => import("react/jsx-runtime").JSX.Element;
-export declare const Github: ({ ...props }: ComponentProps<"div">) => import("react/jsx-runtime").JSX.Element;
-export declare const Npm: ({ ...props }: ComponentProps<"div">) => import("react/jsx-runtime").JSX.Element;
-export declare const WeChat: ({ ...props }: ComponentProps<"div">) => import("react/jsx-runtime").JSX.Element;
