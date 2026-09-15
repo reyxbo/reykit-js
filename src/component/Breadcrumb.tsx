cc5f0097f0e0c4ee4156a097caef5a02da70a96a
ReyXBo <reyxbo@163.com>
2026-09-16 01:00:23 +0800

    Update.

--- src/component/Breadcrumb.tsx
+++ src/component/Breadcrumb.tsx
@@ -5,10 +5,11 @@
  * @Explain : Breadcrumb components.
  */
 
-import { Fragment } from 'react'
+import { ComponentProps, Fragment } from 'react'
 import { useLocation } from 'react-router-dom'
 
 import * as ui from './ui'
+import { cn } from '../lib/twc'
 
 export { type BreadcrumbDict, Breadcrumb }
 
@@ -19,24 +20,29 @@ type BreadcrumbDict = Record<string, { label: string; href?: string }[]>
  * 
  * @param props.dict - Breadcrumb data dictionary.
  */
-function Breadcrumb({ dict } : { dict: BreadcrumbDict }) {
+function Breadcrumb(
+    {
+        dict,
+        ...props
+    } : { dict: BreadcrumbDict } & ComponentProps<typeof ui.Breadcrumb>
+) {
 
     // Parameter.
     const { pathname } = useLocation()
     const data = dict[pathname] || []
 
     return (
-        <ui.Breadcrumb>
-            <ui.BreadcrumbList>
+        <ui.Breadcrumb {...props} className={cn('min-w-0 overflow-hidden', props.className)}>
+            <ui.BreadcrumbList className='min-w-0 flex-nowrap overflow-hidden'>
                 {
                     data.map(
                         (item, index) => (
                             <Fragment key={index}>
-                                <ui.BreadcrumbItem>
+                                <ui.BreadcrumbItem className='min-w-0'>
                                     {
                                         item.href
-                                        ? <ui.BreadcrumbLink href={item.href}>{item.label}</ui.BreadcrumbLink>
-                                        : <ui.BreadcrumbPage>{item.label}</ui.BreadcrumbPage>
+                                        ? <ui.BreadcrumbLink href={item.href} className='truncate'>{item.label}</ui.BreadcrumbLink>
+                                        : <ui.BreadcrumbPage className='truncate'>{item.label}</ui.BreadcrumbPage>
                                     }
                                 </ui.BreadcrumbItem>
                                 {index < data.length - 1 && <ui.BreadcrumbSeparator className='-mx-1 md:mx-0' />}
